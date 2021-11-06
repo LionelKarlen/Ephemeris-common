0849922672227590110724c12841f23a77e37a2e
LionelKarlen <lionel.karlen@protonmail.com>
2021-11-06 22:47:18 +0100

    Make user id optional

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { User } from "./user";
+import { ArchiveCache } from "./archive_cache";
+import { Archive } from "./archive";
+import { EngagementType } from "./engagement_type";
+import { Engagement } from "./engagement";
+import { uuid } from "./uuid";
+import { VisitorType } from "./visitor_type";
+
+export {
+  User,
+  ArchiveCache,
+  Archive,
+  EngagementType,
+  Engagement,
+  uuid,
+  VisitorType,
+};
